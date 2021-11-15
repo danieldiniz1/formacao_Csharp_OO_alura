c32e4306d601cee3847e9bf75c6e5e6d720e019a
Daniel Diniz <daniel.diniz1@icloud.com>
2021-11-15 14:44:19 -0300

    iniciando módulo de exceções

---
+++ 3.entendendo_heranca_interface/Bank/Sistemas/SistemaInterno.cs
@@ -0,0 +1,28 @@
+﻿using Bank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Sistemas
+{
+    public  class SistemaInterno
+    {
+        public bool Logar(IAutenticavel funcionario, string senha)
+        {
+            bool usuarioAutenticado = funcionario.Autenticar(senha);
+
+            if (usuarioAutenticado)
+            {
+                Console.WriteLine("Bem-vindo ao Sistema");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha Inválida, tente novamente!");
+                return false;
+            }
+        }
+    }
+}
