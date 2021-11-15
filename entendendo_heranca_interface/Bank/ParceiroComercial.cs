78fcc1552830a755f80a20f183cd187ba92a7259
Daniel Diniz <daniel.diniz1@icloud.com>
2021-11-14 21:04:31 -0300

    implementando interface com classe abstrata

---
+++ entendendo_heranca_interface/Bank/ParceiroComercial.cs
@@ -0,0 +1,19 @@
+﻿using Bank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class ParceiroComercial : IAutenticavel
+    {
+        public string Senha { get; set; }
+
+        public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
