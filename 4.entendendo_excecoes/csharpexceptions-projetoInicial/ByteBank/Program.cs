4e15b89c291b693e529dd5e96ab742f5053e3208
Daniel Diniz <daniel.diniz1@icloud.com>
2021-11-21 19:38:08 -0300

    Bloco finaly

--- 4.entendendo_excecoes/csharpexceptions-projetoInicial/ByteBank/Program.cs
+++ 4.entendendo_excecoes/csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,55 @@ namespace ByteBank
     class Program
     {
         static void Main(string[] args)
+        {
+            try
+            {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CATCH no Método Main");
+            }
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using(LeitorDeArquivos leitor = new LeitorDeArquivos("contas.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+
+
+            // -----------------------------
+            //LeitorDeArquivos leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivos("contasl.txt");
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+
+            //}
+            //catch (IOException)
+            //{
+
+            //    Console.WriteLine("Exceção do tipo IOException Capturada e tratada");
+            //}
+            //finally
+            //{
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+
+        }
+
+
+
+        private static void TestaInnerException()
         {
             try
             {
@@ -38,7 +88,6 @@ namespace ByteBank
                 Console.WriteLine(ex.Message);
             }
             Console.WriteLine(ContaCorrente.TaxaOperacao);
-            Console.ReadLine();
         }
     }
 }
